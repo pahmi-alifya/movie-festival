ba15671ccd793bbba0ef256ea963e8160bd2936f
Pahmi Alifya Bahri <pahmi.alifya@efishery.com>
2024-11-08 12:55:46 +0700

    feat(home): add navigate to home

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar color="#121212" app dense flat>
-    <v-card-title class="mt-1 text-h5 font-weight-bold"
+    <v-card-title class="mt-1 text-h5 font-weight-bold" @click="redirectToHome"
       >Movie Festival</v-card-title
     >
     <v-spacer></v-spacer>
@@ -11,10 +11,21 @@
 <script>
 import { defineComponent } from "vue";
 import AppBarMenu from "./AppBarMenu.vue";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   components: {
     AppBarMenu,
+  },
+  setup() {
+    const router = useRouter();
+    const redirectToHome = () => {
+      router.push("/");
+    };
+
+    return {
+      redirectToHome,
+    };
   },
 });
 </script>
